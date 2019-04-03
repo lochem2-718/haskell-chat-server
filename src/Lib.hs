170636893fe2bd98091875c4c26356df6bc1f792
Jared Weinberger <jared.j.weinberger@gmail.com>
2019-04-03 12:40:52 -0700

    restructured Lib.hs, removed deprecated

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,13 +4,36 @@ module Lib
 
 import Network.Socket
 
-runServer :: Int -> IO ()
-runServer port_number = do
-    chat_socket <- socket AF_INET Stream 0
-    setSocketOption chat_socket ReuseAddr 1
-    bind chat_socket (SockAddrInet port_number iNADDR_ANY)
-    listen chat_socket 2
-    mainLoop chat_socket
+runServer :: PortNumber -> IO ()
+runServer port =
+    do
+        address <- resolve port
+        socket_ <- open address port
+        mainLoop socket_
+
+resolve :: PortNumber -> IO AddrInfo
+resolve port =
+    let
+        port_string = show port
+        address_info_hint = defaultHints 
+            { addrFlags = [ AI_Passive ]
+            , addrFamily = AF_INET
+            , addrSocketType = Stream
+            }
+    in do
+        address:_ <- getAddrInfo (Just address_info_hint) Nothing (Just port_string)
+        return address
+
+open :: AddrInfo -> PortNumber -> IO Socket
+open address port =
+    let
+        max_queued_connections = 2
+    in do
+        socket_ <- socket (addrFamily address) (addrSocketType address) (addrProtocol address)
+        setSocketOption socket_ ReuseAddr 1
+        bind socket_ $ SockAddrInet port (addrAddress address)
+        listen socket_ max_queued_connections
+        return socket_
 
 mainLoop :: Socket -> IO ()
 mainLoop chat_socket = do
